6e9f54bcd053fe25c2397f45920faa5edc70e763
Diego Magalhães <dgomesbr@gmail.com>
2015-05-25 13:48:30 -0300

    initial import

---
+++ src/main/java/com/diegomagalhaes/felaguiar/trip/FelaguiarTripApplication.java
@@ -0,0 +1,12 @@
+package com.diegomagalhaes.felaguiar.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelaguiarTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelaguiarTripApplication.class, args);
+    }
+}
---
+++ src/test/java/com/diegomagalhaes/felaguiar/trip/FelaguiarTripApplicationTests.java
@@ -0,0 +1,18 @@
+package com.diegomagalhaes.felaguiar.trip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FelaguiarTripApplication.class)
+@WebAppConfiguration
+public class FelaguiarTripApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
