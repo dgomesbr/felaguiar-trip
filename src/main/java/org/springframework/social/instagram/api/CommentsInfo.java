f7b7dacb26c7c9152c76eb4f601c962d30e1b3bc
Diego Magalhães <dgomesbr@gmail.com>
2015-05-25 20:40:38 -0300

    first commit with some basic functionality

---
+++ src/main/java/org/springframework/social/instagram/api/CommentsInfo.java
@@ -0,0 +1,30 @@
+package org.springframework.social.instagram.api;
+
+import java.util.List;
+
+public class CommentsInfo {
+    
+    private final int total;
+    private final List<Comment> list;
+    
+    public CommentsInfo(int total, List<Comment> list) {
+        this.total = total;
+        this.list = list;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public List<Comment> getList() {
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentsInfo{" +
+                "total=" + total +
+                ", list=" + list +
+                '}';
+    }
+}
---
+++ src/main/java/org/springframework/social/instagram/api/Pagination.java
@@ -0,0 +1,35 @@
+package org.springframework.social.instagram.api;
+
+public class Pagination {
+	
+	private String nextUrl;
+	private String nextMaxId;
+	private String nextMinId;
+	
+	public Pagination(String nextUrl, String nextMaxId, String nextMinId) {
+		this.nextUrl = nextUrl;
+		this.nextMaxId = nextMaxId;
+		this.nextMinId = nextMinId;
+	}
+	
+	public String getNextUrl() {
+		return nextUrl;
+	}
+
+	public String getNextMaxId() {
+		return nextMaxId;
+	}
+
+	public String getNextMinId() {
+		return nextMinId;
+	}
+
+	@Override
+	public String toString() {
+		return "Pagination{" +
+				"nextUrl='" + nextUrl + '\'' +
+				", nextMaxId='" + nextMaxId + '\'' +
+				", nextMinId='" + nextMinId + '\'' +
+				'}';
+	}
+}
